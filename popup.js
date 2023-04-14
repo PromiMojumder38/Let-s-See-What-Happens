27ed9b34b450a66b1e0e5686bf313713581aaa3a
PromiMojumder38 <74852911+PromiMojumder38@users.noreply.github.com>
2023-04-14 23:11:02 +0600

    profile update with login

---
+++ popup.js
@@ -0,0 +1,6 @@
+function hidePopup() {
+    var popupSuccess = document.getElementById("popup-success");
+    var popupError = document.getElementById("popup-error");
+    popupSuccess.style.display = "none";
+    popupError.style.display = "none";
+  }
