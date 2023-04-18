3802b0a47ec2a108a3f0ed95118aaf55ff0a63b7
PromiMojumder38 <74852911+PromiMojumder38@users.noreply.github.com>
2023-04-18 11:32:01 +0600

    martyrs problem added login registration edit with firebase and toggle

--- popup.js
+++ popup.js
@@ -4,3 +4,28 @@ function hidePopup() {
     popupSuccess.style.display = "none";
     popupError.style.display = "none";
   }
+
+  function hidePopup2() {
+    var popupSuccess2 = document.getElementById("popup-success2");
+    var popupError2 = document.getElementById("popup-error2");
+    popupSuccess2.style.display = "none";
+    popupError2.style.display = "none";
+  }
+
+  function toggle() {
+    var registrationForm = document.getElementById("registration");
+    var loginForm = document.getElementById("loginn");
+
+      registrationForm.style.display = "none";
+      loginForm.style.display = "block";
+  }
+
+  function toggle2() {
+    var registrationForm = document.getElementById("registration");
+    var loginForm = document.getElementById("loginn");
+
+      registrationForm.style.display = "block";
+      loginForm.style.display = "none";
+  }
+
+  
